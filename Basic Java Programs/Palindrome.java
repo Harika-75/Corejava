01fdc1ceb138a856fbcefcd0409b0a4573c12fed
Harika Baddukonda <147475012+Harika-75@users.noreply.github.com>
2025-12-02 23:10:12 +0530

    Implement palindrome check in Palindrome.java

---
+++ Basic Java Programs/Palindrome.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n=sc.nextInt();
+		int org=n;
+		int rev=0;
+		while(n>0){
+		    int r=n%10;
+		    rev=rev*10+r;
+		    n=n/10;
+		}
+		if(org==rev){
+		    System.out.println("Palindrome Number");
+		}
+		else System.out.println(" Not a Palindrome Number");
+	}
+}
